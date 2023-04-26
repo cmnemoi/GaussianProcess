e6bb77a3a9c868e524984a6c7b9d4d0a274b32fa
Charles-Meldhine Madi Mnemoi <icdk5b4zf@relay.firefox.com>
2023-04-26 18:52:50 +0200

    add model comparaison on test set and some interpretation

--- plot.py
+++ plot.py
@@ -1,3 +1,5 @@
+from typing import Tuple
+
 import numpy as np
 import matplotlib.pyplot as plt # Graphiques (bas niveau)
 import seaborn as sns # Graphiques (haut niveau)
@@ -33,3 +35,37 @@ def plot_train_and_val(
     axes[1].set_ylabel("Logarithme du revenu")
     axes[0].legend(['Données d\'entraînement', 'Prédiction'])
     axes[1].legend(['Données de validation', 'Prédiction'], loc='lower right')
+
+def plot_confidence_and_interval(gp, X: np.ndarray, y: np.ndarray) -> Tuple[np.ndarray, np.ndarray]:
+    """Plot the prediction and the 95% confidence interval for the given Gaussian process
+    gp: Gaussian process
+    X: Input values
+    y: Output values
+
+    Returns:
+    lower_bound: Lower bound of the confidence interval
+    upper_bound: Upper bound of the confidence interval
+    """
+    mean_prediction, std_prediction = gp.predict(X)
+    lower_bound = mean_prediction - 1.96 * np.sqrt(np.diag(std_prediction))
+    upper_bound = mean_prediction + 1.96 * np.sqrt(np.diag(std_prediction))
+    unique = np.unique(X, return_index=True)[1] # avoid a bug with plt.fill_between
+
+    plt.figure(figsize=(8, 6))
+    sns.scatterplot(x=X, y=y, color="tab:blue", label="Données")
+    sns.lineplot(x=X, y=mean_prediction, color="tab:red", label="Prédiction")
+    plt.fill_between(
+        X.ravel()[unique],
+        lower_bound.ravel()[unique],
+        upper_bound.ravel()[unique],
+        color="tab:orange",
+        alpha=0.5,
+        label=r"95% confidence interval",
+    )
+    plt.xlabel("Âge en années")
+    plt.ylabel("Logarithme du revenu")
+    plt.legend()
+    plt.title("Prédiction et intervalle de confiance à 95%")
+
+    return lower_bound, upper_bound
+    
